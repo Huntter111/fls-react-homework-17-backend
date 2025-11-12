72a1b3b9374be927261fe0369c5e155ead315a27
Hunter111 <sukhomudrenkov17@gmail.com>
2025-11-12 23:40:23 +0200

    refactor: improve CORS configuration to include production frontend URL

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const app = express()
 
 // app.use(cors({ origin: 'http://localhost:5173/', credentials: true }))
 app.use(
-  cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // ✅ фронтенд (Vite)
-    credentials: true, // ✅ дозвіл надсилати cookie
-  })
+	cors({
+		origin: ['http://localhost:5173', 'http://localhost:5174', 'https://fls-react-homework-17.netlify.app'], // ✅ фронтенд (Vite)
+		credentials: true, // ✅ дозвіл надсилати cookie
+	}),
 )
 
 app.use(express.json())
